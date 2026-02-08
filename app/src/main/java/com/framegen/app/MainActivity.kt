95e07fbeb8c7aa577d21710c9d64d5c960c9deb6
proqaz2-design <proqaz2@gmail.com>
2026-02-08 19:11:06 +0000

    fix: qualify isActive to avoid CoroutineScope.isActive shadowing

--- app/src/main/java/com/framegen/app/MainActivity.kt
+++ app/src/main/java/com/framegen/app/MainActivity.kt
@@ -505,7 +505,7 @@ class MainActivity : AppCompatActivity() {
             // Step 2: Start the background service
             FrameGenService.start(this@MainActivity)
 
-            isActive = true
+            this@MainActivity.isActive = true
             updateUI()
             startStatsMonitor()
 
@@ -523,7 +523,7 @@ class MainActivity : AppCompatActivity() {
 
             FrameGenService.stop(this@MainActivity)
             statsJob?.cancel()
-            isActive = false
+            this@MainActivity.isActive = false
             updateUI()
         }
     }
